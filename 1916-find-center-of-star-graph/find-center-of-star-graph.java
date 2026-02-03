188047d26d827f5bbe2a198e0c4154083fb71a05
shrinarayan99 <yadavsanskar9936@gmail.com>
2026-02-03 15:10:14 +0530

    Time: 18 ms (18.31%) | Memory: 140.6 MB (5.01%) - LeetSync

---
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int findCenter(int[][] edges) {
+        int m=edges.length;
+        int nodes=m+1;
+        ArrayList[]  arr=new ArrayList[nodes+1];
+        for(int i=0;i<=nodes;i++){
+            arr[i]=new ArrayList<>();
+        }
+        for(int i=0;i<m;i++){
+            arr[edges[i][0]].add(edges[i][1]);
+            arr[edges[i][1]].add(edges[i][0]);
+        }
+        int ans=0;
+        for(int i=0;i<=nodes;i++){
+            if(arr[i].size()==m){
+                ans=i;
+                break;
+            }
+        }
+        return ans;
+    }
+}
+/* Adjecy matrix(through MLE and TLE both)
+
+
+class Solution {
+    public int findCenter(int[][] edges) {
+        int nodes=edges.length+1;
+        int[][] dp=new int[nodes+1][nodes+1];
+        for(int i=0;i<edges.length;i++){
+            dp[edges[i][0]][edges[i][1]]=1;
+            dp[edges[i][1]][edges[i][0]]=1;
+        }
+        int count=0;
+        int ans=-1;
+        for(int i=1;i<=nodes;i++){
+            for(int val:dp[i]){
+                count+=val;
+            }
+            if(count==nodes-1){
+                ans=i;
+                break;
+            }
+            count=0;
+        }
+        return ans;
+    }
+}
+
+*/
