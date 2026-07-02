e3e09a45f5948401b4ed77662adc07cba976ef6a
Shri Narayan <yadavsanskar9936@gmail.com>
2026-07-02 16:48:40 +0530

    Time: 12 ms (80.74%) | Memory: 47 MB (70.49%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
@@ -1,46 +1,35 @@
 class Solution {
-    public static int[] dir_X={-1,1,0,0};
-    public static int[] dir_Y={0,0,-1,1};
+    static int[] X={0,0,-1,1};
+    static int[] Y={-1,1,0,0};
     public boolean findSafeWalk(List<List<Integer>> grid, int health) {
-        int row=grid.size();
-        int col=grid.get(0).size();
+        int m=grid.size();
+        int n=grid.get(0).size();
 
-        int[][] health_require=new int[row][col];
+        int[][] dp=new int[m][n];
+        dp[0][0]=health-grid.get(0).get(0);
+        PriorityQueue<int[]> q=new PriorityQueue<>((a,b)->b[0]-a[0]);
 
-        for(int i=0;i<row;i++){
-            Arrays.fill(health_require[i],Integer.MAX_VALUE);
-        }
-        health_require[0][0]=grid.get(0).get(0);
+        q.add(new int[] {health-grid.get(0).get(0),0,0});
 
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
-        //(healthRequired,i,j)
-        pq.add(new int[] {grid.get(0).get(0),0,0});
-
-        while(!pq.isEmpty()){
-            int[] curr=pq.poll();
-            int heal=curr[0];
+        while(!q.isEmpty()){
+            int[] curr=q.poll();
+            int hel=curr[0];
             int x=curr[1];
             int y=curr[2];
-
-            if(health_require[x][y]<heal) continue;
-
+            if(hel>0 && x==m-1 && y==n-1) return true;
             for(int i=0;i<4;i++){
-                int nx=x+dir_X[i];
-                int ny=y+dir_Y[i];
+                int nx=x+X[i];
+                int ny=y+Y[i];
 
-                if(nx<0 || ny<0 || nx>=row || ny>=col) continue;
+                if(nx<0 || ny<0 || nx>m-1 || ny>n-1) continue;
 
-                int heal_red=heal+grid.get(nx).get(ny);
-
-                if(health_require[nx][ny]>heal_red){
-                    health_require[nx][ny]=heal_red;
-                    pq.add(new int[] {heal_red,nx,ny});
+                int currHel=hel-grid.get(nx).get(ny);
+                if(dp[nx][ny]<currHel){
+                    dp[nx][ny]=currHel;
+                    q.add(new int[] {currHel,nx,ny});
                 }
             }
         }
-        int dif=health-health_require[row-1][col-1];
-
-        if(dif>0) return true;
         return false;
     }
 }
