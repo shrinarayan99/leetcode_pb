9a5524b119a56353baed18e2f50851572850000d
Shri Narayan <yadavsanskar9936@gmail.com>
2026-09-24 23:38:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (8.50%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,67 +1,72 @@
-class Solution {
+// class Solution {
 
-    int[][] dp;
+//     int[][] dp;
 
-    public int rob(int[] nums) {
+//     public int rob(int[] nums) {
 
-        // Case 1: first house NOT robbed
-        dp = new int[nums.length][2];
-        for (int i = 0; i < nums.length; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+//         // Case 1: first house NOT robbed
+//         dp = new int[nums.length][2];
+//         for (int i = 0; i < nums.length; i++) {
+//             Arrays.fill(dp[i], -1);
+//         }
 
-        int case1 = sol(1, nums, 0, 0);
+//         int case1 = sol(1, nums, 0, 0);
 
-        // Case 2: first house robbed
-        dp = new int[nums.length][2];
-        for (int i = 0; i < nums.length; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+//         // Case 2: first house robbed
+//         dp = new int[nums.length][2];
+//         for (int i = 0; i < nums.length; i++) {
+//             Arrays.fill(dp[i], -1);
+//         }
 
-        int case2 = nums[0] + sol(1, nums, 1, 1);
+//         int case2 = nums[0] + sol(1, nums, 1, 1);
 
-        return Math.max(case1, case2);
-    }
+//         return Math.max(case1, case2);
+//     }
 
-    // prevRobbed: 0 = not robbed, 1 = robbed
-    // firstRobbed: 0 = not robbed, 1 = robbed
+//     // prevRobbed: 0 = not robbed, 1 = robbed
+//     // firstRobbed: 0 = not robbed, 1 = robbed
 
-    public int sol(int i, int[] nums, int firstRobbed, int prevRobbed) {
+//     public int sol(int i, int[] nums, int firstRobbed, int prevRobbed) {
 
-        if (i >= nums.length)
-            return 0;
+//         if (i >= nums.length)
+//             return 0;
 
-        if (dp[i][prevRobbed] != -1)
-            return dp[i][prevRobbed];
+//         if (dp[i][prevRobbed] != -1)
+//             return dp[i][prevRobbed];
 
-        // If first house was robbed, last house cannot be robbed
-        if (i == nums.length - 1 && firstRobbed == 1)
-            return 0;
+//         // If first house was robbed, last house cannot be robbed
+//         if (i == nums.length - 1 && firstRobbed == 1)
+//             return 0;
 
-        int skip = sol(i + 1, nums, firstRobbed, 0);
+//         int skip = sol(i + 1, nums, firstRobbed, 0);
 
-        int take = 0;
+//         int take = 0;
 
-        if (prevRobbed == 0) {
-            take = nums[i] + sol(i + 1, nums, firstRobbed, 1);
-        }
+//         if (prevRobbed == 0) {
+//             take = nums[i] + sol(i + 1, nums, firstRobbed, 1);
+//         }
 
-        return dp[i][prevRobbed] = Math.max(take, skip);
-    }
-}
-/*
+//         return dp[i][prevRobbed] = Math.max(take, skip);
+//     }
+// }
+// /*
 
 class Solution {
     int[][][] dp;
     public int rob(int[] nums) {
         dp=new int[nums.length][2][2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<2;j++){
+                Arrays.fill(dp[i][j],-1);
+            }
+        }
         return Math.max(sol(1,nums,0,0),nums[0]+sol(1,nums,1,1));
     }
     //prevrobbed: {0} not robbed {1} robbed
     //firstRobbed: {0} not robbed {1} robbed
     public int sol(int i,int[] nums,int firstRobbed,int prevRobbed){
         if(i>=nums.length) return 0;
-        if(dp[i][firstRobbed][prevRobbed]!=0) return dp[i][firstRobbed][prevRobbed];
+        if(dp[i][firstRobbed][prevRobbed]!=-1) return dp[i][firstRobbed][prevRobbed];
         if(i==nums.length-1 && firstRobbed==1) return 0;
         int take=0;
         int skip=sol(i+1,nums,firstRobbed,0);
@@ -71,4 +76,4 @@ class Solution {
         return dp[i][firstRobbed][prevRobbed]=Math.max(take,skip);
     }
 }
-*/
+//*/
